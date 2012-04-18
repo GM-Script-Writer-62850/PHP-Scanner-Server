184fb170307af1550345925953ade7e25e5dea95
pqwoerituytrueiwoq <pqwoerituytrueiwoq@gmail.com>
2012-04-18 09:56:42 -0400

    Initial commit

---
+++ inc/footer.php
@@ -0,0 +1,5 @@
+<div id="footer">
+<p><small><a class="tool" href="http://ubuntuforums.org/showthread.php?t=1519201&page=19"><?php echo html($GLOBALS['NAME']); ?><span class="tip">Help and Support</span></a> version <a class="tool" href="download.php?ver=<?php echo html($GLOBALS['VER']); ?>&downloadServer"><?php echo $GLOBALS['VER']; ?><span class="tip">Download</span></a> is
+running on <a href="/"><?php echo html($_SERVER['SERVER_NAME']); ?></a> and there are <a href="index.php?page=About">release notes</a> advailable.</small></p>
+</div>
+</div>
