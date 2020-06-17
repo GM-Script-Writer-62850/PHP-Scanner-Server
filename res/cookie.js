40c63ac5fb141b795fd7772e8e1bb45d51cca899
GM-Script-Writer-62850 <pqwoerituytrueiwoq@gmail.com>
2020-06-17 13:58:41 -0400

    cookies need to work without https
    Just cause the browser error message is gone does not mean it was fixed...

--- res/cookie.js
+++ res/cookie.js
@@ -12,12 +12,12 @@ function Set_Cookie( name, value, expires, path, domain, secure ){
 		( ( expires ) ? ";expires=" + expires_date.toGMTString() : "" ) +
 		( ( path ) ? ";path=" + path : "" ) +
 		( ( domain ) ? ";domain=" + domain : "" ) +
-		";secure";
+		( ( secure ) ? ";secure" : ";samesite=strict" );
 }
 function Delete_Cookie( name, path ) {// edited, don't delete non-existent cookies
 	if(!path) path=window.location.pathname.substr(0,window.location.pathname.lastIndexOf('/')+1);// Added this line
 	document.cookie = name + "=" +
 		( ( path ) ? ";path=" + path : "") +
 		";domain=" + document.domain +
-		";expires=Thu, 01-Jan-1970 00:00:01 GMT;secure";
+		";expires=Thu, 01-Jan-1970 00:00:01 GMT";
 }
